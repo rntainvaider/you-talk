7a1e5fdd4a4785b62ae30336772a483f0e1a737b
evgenii <vovk.evgenii.199811@gmail.com>
2025-04-17 16:41:56 +0300

    доделать на страничке блог 6 карточек

--- assets/script/hidden.js
+++
@@ -1,6 +0,0 @@
-const headerButtonHidden = document.querySelector(".header__button-hidden");
-const headerHidden = document.querySelector(".header__hidden");
-
-headerButtonHidden.addEventListener("click", () => {
-    headerHidden.classList.toggle("hidden");
-})
